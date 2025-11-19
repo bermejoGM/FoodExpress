6f41e5e11be43acaf3557f2b13bb63f160592a6d
DiegoBermejoGarciaMora <diegobermejogarciamora@gmail.com>
2025-11-19 20:58:26 +0100

    primer commit

---
+++ FoodExpressMVC/src/main/java/es/daw/foodexpressmvc/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package es.daw.foodexpressmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+Cuando alguien acceda a estas rutas, no llames a ningún controlador, simplemente renderiza directamente la vista indicada.
+Creas mapeos sin lógica de negocio.
+Es ideal para páginas estáticas o de presentación.
+NO USAR Cuando la vista necesita datos dinámicos, entonces sí necesitas un @Controller.
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home"); // AHORA LA PAGINA PRINCIPAL ES home.html
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
+        //registry.addViewController("/dashboard").setViewName("dashboard"); // Esta hecho en DashboardController
+    }
+}
---
+++ FoodExpressMVC/src/main/java/es/daw/foodexpressmvc/service/RestaurantsService.java
@@ -0,0 +1,41 @@
+package es.daw.foodexpressmvc.service;
+
+import es.daw.foodexpressmvc.dto.RestaurantDTO;
+import es.daw.foodexpressmvc.exception.ConnectionApiRestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantsService {
+
+    private final WebClient webClientAPI;
+
+    public List<RestaurantDTO> getAllRestaurants(){
+
+        RestaurantDTO[] restaurants;
+
+        try {
+            restaurants = webClientAPI
+                    .get()
+                    .uri("/restaurants")
+                    .retrieve()
+                    .bodyToMono(RestaurantDTO[].class)
+                    .block(); //asíncrono
+        }catch (Exception e){
+            // Pendiente crear excepción propia
+            // Pendiente crear Globla ExceptionHancler: que lea la exceión y redirija a api-error
+            //
+            throw new ConnectionApiRestException("Could not connect to FoodExpress API");
+        }
+
+        return Arrays.asList(restaurants);
+
+
+
+    }
+}
